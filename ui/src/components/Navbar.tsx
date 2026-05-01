776b0e5cbc1421ccf1206fbb10d98cfe0f37f09b
Rajkumar <thecodeheist01@gmail.com>
2026-05-01 15:37:19 +0530

    basic_initial_nav_added

--- ui/src/components/Navbar.tsx
+++ ui/src/components/Navbar.tsx
@@ -1,24 +1,36 @@
+import { Link } from "react-router-dom"
+import NightModeIcon from "../svgs/NightModeIcon"
 
 function Navbar() {
-  return (
-    <div className="py-10 w-full flex items-center">
-     
-     <div className="flex justify-between w-full">
-        
-        {/* left side links */}
-        <div>  </div>
+    return (
+        <div className="py-10 w-full flex items-center">
 
-        {/* right side links */}
-        <div className="">
-            <button>  </button>
+            <div className="flex justify-between w-full">
+
+                {/* left side links */}
+                <div className="flex gap-6 items-center font-semibold tracking-wider">
+                    <Link to={"/"}>Me</Link>
+                    <div className=" bg-gray-600 h-5 w-px "></div>
+                    <Link to={"/work"}>Work</Link>
+                    <div className=" bg-gray-600 h-5 w-px "></div>
+                    <Link to={"/posts"}>Posts</Link>
+
+                </div>
+
+                {/* right side links */}
+                <div className="">
+                    <button className=" cursor-pointer"> <NightModeIcon /> </button>
+                </div>
+
+
+            </div>
+
+
         </div>
-
-
-     </div>
-
-
-    </div>
-  )
+    )
 }
 
 export default Navbar
+
+
+
--- ui/src/screens/Me.tsx
+++ ui/src/screens/Me.tsx
@@ -4,7 +4,7 @@ function Me() {
   return (
     <div className="flex justify-center bg-[#0F1010] text-white min-h-screen">
 
-        <div className="flex flex-col min-h-screen justify-between sm:-[80%] md:w-[70%]">
+        <div className="flex flex-col min-h-screen justify-between sm:-[80%] md:w-[70%] lg:w-[50%]">
 
             <Navbar />
 
